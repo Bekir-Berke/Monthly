6e9ff7add0c9c43545cadbd66003ed6312345c2d
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-14 22:13:30 +0300

    create app

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Bekir Berke Yılmaz on 14.10.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
