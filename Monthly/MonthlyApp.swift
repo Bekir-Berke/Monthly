a0ab2767534388a0bfbf6a5d72b1f2eb08922fd5
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-14 19:41:54 +0300

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Bekir Berke Yılmaz on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
